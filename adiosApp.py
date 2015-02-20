6991d94320964f021dd512bc050e6be1bdc8be7c
nachomanriqueperez <joseim@zeta20.aulas.gsyc.es>
2015-02-20 13:12:18 +0100

    Arreglado fallo

--- adiosApp.py
+++ adiosApp.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import webapp;
 
-class miservi(webapp.webApp):
+class adiosApp(webapp.webApp):
 
     def process(self,parsedRequest):
         """Parse"""
         return ("200 OK","<html><body><b>Adios mundo cruel!</h1></body></html>")
 
 if __name__ == "__main__":
-    testWebApp = miservi("localhost",1234);
+    testWebApp = adiosApp("localhost",1234);
--- holaApp.py
+++ holaApp.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import webapp;
 
-class miservi(webapp.webApp):
+class holaApp(webapp.webApp):
 
     def process(self,parsedRequest):
         """Parse"""
         return ("200 OK","<html><body><b>Hola Mundo!</h1></body></html>")
 
 if __name__ == "__main__":
-    testWebApp = miservi("localhost",1234);
+    testWebApp = holaApp("localhost",1234);
